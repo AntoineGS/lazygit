8d5d978efb5f4bec8b8aebf2e11a4247d56e4a1c
Stefan Haller <stefan@haller-berlin.de>
2026-04-06 19:59:32 +0200

    Copy gocui files into lazygit repo under pkg/gocui
    I copied all files except dot files (.github and .gitignore), the _examples
    folder, and go.mod/go.sum.
    
    At some point we may want to copy the files back to the gocui repo when other
    clients (e.g. lazydocker) want to use the newer versions of them.

--- pkg/gui/controllers.go
+++ pkg/gui/controllers.go
@@ -3,8 +3,8 @@ package gui
 import (
 	"strings"
 
-	"github.com/jesseduffield/gocui"
 	"github.com/jesseduffield/lazygit/pkg/commands/models"
+	"github.com/jesseduffield/lazygit/pkg/gocui"
 	"github.com/jesseduffield/lazygit/pkg/gui/controllers"
 	"github.com/jesseduffield/lazygit/pkg/gui/controllers/helpers"
 	"github.com/jesseduffield/lazygit/pkg/gui/services/custom_commands"
